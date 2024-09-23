362890d70732449870a37c4cbc63ad907a835128
kopiPejst92 <agkop01@gmail.com>
2024-09-23 15:12:17 +0200

    Added bootstrap and add/delete functionalities

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Appointment } from '../models/appointment';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.css']
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit {
 
+  newAppointmentTitle: string = "";
+  newAppointmentDate: Date = new Date();
+
+  appointments: Appointment[] = []
+
+  ngOnInit(): void {
+    let savedAppointments = localStorage.getItem("appointments")
+    this.appointments = savedAppointments ? JSON.parse(savedAppointments) : []
+  }
+
+  addAppointment() {
+    if (this.newAppointmentTitle.trim().length && this.newAppointmentDate) {
+      let newAppointment: Appointment = {
+        id: this.appointments.length + 1,
+        title: this.newAppointmentTitle,
+        date: this.newAppointmentDate
+      }
+      this.appointments.push(newAppointment)
+      this.newAppointmentTitle = "";
+      this.newAppointmentDate = new Date();
+
+      localStorage.setItem("appointments", JSON.stringify(this.appointments))
+    }
+  }
+
+  deleteAppointment(index: number) {
+    this.appointments.splice(index, 1)
+    localStorage.setItem("appointments", JSON.stringify(this.appointments))
+  }
 }
+

c4208d632642944fd8392fac7583df34527bd12f
kopiPejst92 <agkop01@gmail.com>
2024-09-25 11:13:50 +0200

    adding css style

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -3,7 +3,7 @@ import { Appointment } from '../models/appointment';
 
 @Component({
   selector: 'app-appointment-list',
-  templateUrl: './appointment-list.component.html',
+  templateUrl: './appointment-list.component.html', 
   styleUrls: ['./appointment-list.component.css']
 })
 export class AppointmentListComponent implements OnInit {
